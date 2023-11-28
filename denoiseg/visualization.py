7199568901e8ebb7b2cb38517d7fa8e052df01cd
Jaroslav Knotek <knotekjaroslav@email.cz>
2023-11-28 18:56:00 +0100

    add skeleton

---
+++ denoiseg/visualization.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+
+def plot_row(imgs, vmin_vmax=None, cell_size=5):
+    _, axs = plt.subplots(1, len(imgs), figsize=(cell_size * len(imgs), cell_size))
+
+    for ax, im in zip(axs, imgs):
+        if vmin_vmax is not None:
+            ax.imshow(im, vmin=vmin_vmax[0], vmax=vmin_vmax[1])
+        else:
+            ax.imshow(im)
